0280c6c76db4f74ecfd271d1517ebdb04f988194
Henrique Melo <henrimelo10@gmail.com>
2019-09-25 11:09:25 -0300

    Removes old definitions from ultrasonic.

--- car/UltrasonicDriver.h
+++ car/UltrasonicDriver.h
@@ -46,23 +46,3 @@ class UltrasonicDriver
 };
 
 #endif
-
-/* class UltrasonicDriver
-{
-public:
-    UltrasonicDriver(PinName tx, PinName rx);
-    float read();
-
-private:
-    DigitalInOut _tx;
-    InterruptIn _rx;
-    Timer t;
-    Ticker triger;
-    float eco;
-    void triger_func(void);
-    void start_func(void);
-    void end_func(void);
-};
-
-#endif
- */
